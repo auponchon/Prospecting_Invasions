27ae996c9114b489c05054a00c87476ecf8a4e66
auponchon <aurore.ponchon@gmail.com>
2021-10-21 19:03:46 +0200

    2021-10-21-1

--- Suppl mat 3 - Gif of invasion fronts.R
+++ Suppl mat 3 - Gif of invasion fronts.R
@@ -14,21 +14,21 @@ names(anima)[c(13,15)]<-c("Perceptual_Range","Preference")
 
 gganim<-ggplot(anima,aes(x=y,y=x))+
     geom_tile(aes(fill=log(Nadult))) +
-    facet_grid(Preference + Scen ~ Perceptual_Range , labeller = "label_both") + 
+    facet_grid(Scen + Preference ~ Perceptual_Range , labeller = "label_both") + 
     scale_fill_gradientn(limits=c(0,5),labels=seq(0,160,32),
                          colours=plasma(100),
                          aesthetics = "fill") +
     scale_y_continuous(breaks=seq(0,20,5),expand = c(0.01,0.01)) +
     scale_x_continuous(breaks=seq(0,220,50),labels=seq(0,220,50),expand = c(0.01, 0.01)) +
-   labs(x="y",y="x", fill = "Nb of adults") +
+   labs(x="Distance from first row",y="x", fill = "Nb of adults") +
     theme_bw() +
     theme(legend.position = "right",
           legend.title = element_text(size=8),
           axis.text = element_text(size=7),
           legend.text = element_text(size=7),
           panel.spacing = unit(0.5, "lines"),
-          strip.text.x = element_text(size = 7),
-          strip.text.y = element_text(size = 6))
+          strip.text.x = element_text(size = 6),
+          strip.text.y = element_text(size = 5))
 #  guides(fill = guide_colorbar(barwidth = 1, barheight = 15)) +
 
 #print(gganim)
